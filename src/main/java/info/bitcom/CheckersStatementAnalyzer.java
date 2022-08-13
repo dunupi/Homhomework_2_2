ef040d6f0617c566113147e33a5e8b892709a3a2
evdokimov aleksandr <dunupi@mail.ru>
2022-08-13 17:03:37 +1000

    MultipleVariables test passed

--- src/main/java/info/bitcom/CheckersStatementAnalyzer.java
+++ src/main/java/info/bitcom/CheckersStatementAnalyzer.java
@@ -1,15 +1,27 @@
 package info.bitcom;
 
+import java.util.HashMap;
+import java.util.Map;
+import static java.util.Map.Entry;
+
 public class CheckersStatementAnalyzer {
 
-    private String variableValue;
+    private Map<String,String> variables;
+    private String inputText;
     public CheckersStatementAnalyzer(String inputText){
+        this.variables = new HashMap<>();
+        this.inputText = inputText;
     }
-    public void set(String value) {
-        this.variableValue = value;
+    public void set(String coord, String value) {
+        this.variables.put(coord,value);
     }
 
     public String evaluate() {
-        return variableValue;
+        String result = inputText;
+        for (Entry<String,String> entry : variables.entrySet()) {
+            String reegex = "\\$\\{" + entry.getKey() + "\\}";
+            result = result.replaceAll(reegex, entry.getValue());
+        }
+        return result;
     }
 }
--- src/test/java/info/bitcom/CheckersStatementAnalyzerTest.java
+++ src/test/java/info/bitcom/CheckersStatementAnalyzerTest.java
@@ -3,21 +3,17 @@ package info.bitcom;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
 
 public class CheckersStatementAnalyzerTest {
-    @Test
-    public void oneVariable() throws Exception{
-        CheckersStatementAnalyzer analyzer = new CheckersStatementAnalyzer("a1");
-        analyzer.set("a1");
-        assertEquals("a1", analyzer.evaluate());
 
-    }
+
 
     @Test
-    public void differentValue() throws Exception {
-        CheckersStatementAnalyzer analyzer = new CheckersStatementAnalyzer("a3");
-        analyzer.set("a3");
-        assertEquals("a3", analyzer.evaluate());
+    public void multipleVariables() throws Exception {
+        CheckersStatementAnalyzer analyzer = new CheckersStatementAnalyzer("${one}, ${two}, ${three}");
+        analyzer.set("one", "a1");
+        analyzer.set("two", "a3");
+        analyzer.set("three", "b2");
+        assertEquals("a1, a3, b2",analyzer.evaluate());
     }
 }
