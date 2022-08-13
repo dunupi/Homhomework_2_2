69d8f582e1f8aa156523f2a0887f60e58ab44979
ID <nabei@bitcom.info>
2022-08-13 13:46:12 +1000

    Search-and-replace for handling multiple variables

--- src/main/java/info/bitcom/CheckersStatementAnalyzer.java
+++ src/main/java/info/bitcom/CheckersStatementAnalyzer.java
@@ -1,15 +1,31 @@
 package info.bitcom;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 public class CheckersStatementAnalyzer {
 
-    private String variableValue;
-    public CheckersStatementAnalyzer(String inputText){
+    private final Map<String, String> coordinates;
+    private String inputText;
+
+
+    public CheckersStatementAnalyzer(String inputText) {
+        this.coordinates = new HashMap<>();
+        this.inputText = inputText;
+
     }
-    public void set(String value) {
-        this.variableValue = value;
+
+    public void set(String inputText, String value) {
+        this.coordinates.put(inputText,value);
     }
 
     public String evaluate() {
-        return variableValue;
+        String result = inputText;
+        for (Entry<String,String> entry : coordinates.entrySet()){
+            String regex = "\\$\\{" + entry.getKey() + "\\}";
+            result = result.replaceAll(regex, entry.getValue());
+        }
+        return result;
     }
 }
